d6dade19febd159661f82da0c9c786fcbf9a4206
Hossein <hossienbehzadi1381@gmail.com>
2023-11-09 00:09:23 +0330

    add models category article and auther

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,46 @@
 from django.db import models
+from django.contrib.auth.models import User
+from ckeditor.fields import RichTextField
+from datetime import datetime
 
-# Create your models here.
+
+
+def validate_file_extentions(value):
+    import os
+    from django.core.exceptions import ValidationError
+    ext = os.path.splitext(value.name)[1]
+    valid_extention = ['.jpg', '.png' ,'.jpeg']
+    if not ext.lower() in valid_extention:
+        raise ValidationError("Unsupported file")
+
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    avatar = models.FileField(upload_to='files/useravatar', null=True, blank=True ,validators=[validate_file_extentions])
+    description = models.CharField(max_length=512, null=False, blank=False)
+
+    def __str__(self):
+        return self.user.first_name + " " + self.user.last_name
+
+
+class Article(models.Model):
+    title = models.CharField(max_length=128, null=False,blank=False)
+    cover = models.FileField(upload_to='files/article_cover', null=False,blank=False, validators=[validate_file_extentions])
+    content = RichTextField()
+    crated_at = models.DateTimeField(default=datetime.now, blank=False)
+    category = models.ForeignKey('Category', on_delete=models.CASCADE)
+    author = models.OneToOneField(UserProfile, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+class Category(models.Model):
+    title = models.CharField(max_length=128, null=False ,blank=False)
+    cover = models.FileField(upload_to='files/category_cover', null=False,blank=False, validators=[validate_file_extentions])
+
+    def __str__(self):
+        return self.title
+
+
+
+
--- blogpy/settings.py
+++ blogpy/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os.path
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'ckeditor',
     'blog',
 ]
 
@@ -120,9 +121,19 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+#ckeditor srttings
+STATIC_ROOT = os.path.dirname(os.path.abspath(__file__))
+STATIC_URL = 'static/'
+
+
+CKEDITOR_BASEPATH = "/static/ckeditor/ckeditor/"
+CKEDITOR_UPLOAD_PATH = "uploads/"
+
+
